a4d6e186ee355c7e01f2a08dd1335599d40d7c8b
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2022-07-28 09:56:37 -0300

    resolucao dos topicos 1 a 3

--- Modulo_01-Fundamentos/Bloco_05-Javascript-DOM-Eventos-WebStorage/dia_01/estilizacao-exercicios.js
+++ Modulo_01-Fundamentos/Bloco_05-Javascript-DOM-Eventos-WebStorage/dia_01/estilizacao-exercicios.js
@@ -1,2 +1,21 @@
+function mudarOTexto (){
+    let segundoP = document.getElementsByTagName("p")[1];
+    segundoP.innerHTML = "Daqui a dois anos estarei trabalhando em uma empresa cujos valores compartilho, ganhando cerca de 4k.";
+}
+
+mudarOTexto();
+
+function verdeTrybe () {
+    let primeiroMainContent = document.getElementsByClassName("main-content")[0];
+    primeiroMainContent.style.backgroundColor = "rgb(76,164,109)";
+}
+verdeTrybe();
+
+function caixaDoMeio () {
+    let primeiroMainContent = document.getElementsByClassName("center-content")[0];
+    primeiroMainContent.style.backgroundColor = "white";
+}
+
+caixaDoMeio()
 
 
